1da5890c34f3d8ab28fae1b170e327e59ac8431b
nmice <39015134+nmice@users.noreply.github.com>
2018-05-08 03:59:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 package com.task;
-//Task01_003 - Составить программу вывода на экран числа, вводимого с клавиатуры.
-// Выводимому числу должно предшествовать сообщение "Вы ввели число".
+/* Task01_003 - Create a program to display the number entered on the keyboard.
++The displayed number must be preceded by the message "You entered a number." */
 
-        import java.io.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
-        System.out.println("Вы ввели число " + n);
-    }
+public static void main(String[] args) throws Exception {
+   BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+   int n = Integer.parseInt(reader.readLine());
+   System.out.println("You entered a number " + n);
+   }
 }
