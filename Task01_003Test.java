27e692d908c04c6f3d55cacf522110d81bc3a4e2
nmice <39015134+nmice@users.noreply.github.com>
2018-05-13 05:14:38 +0300

    Create  Task01_003Test.java

---
+++ Task01_003Test.java
@@ -0,0 +1,24 @@
+package com.task;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mike Neginsky on 13.01.2016.
+ */
+public class Task01_003Test {
+
+    private Task01_003 task
+
+    @Before
+    public void init() { task = new Task01_003(); }
+
+    @After
+    public void tearDown() { task = null; }
+
+    @Test
+    public void todo() {
+        assertTrue(main(1) == "You entered a number 1");
+    }
+}
