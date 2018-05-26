7072884b530a3cbf1af7eeff281b0c6a51365140
n.mice <n.mice@mail.ru>
2018-05-26 13:22:49 +0300

    Task04_036 done

--- src/week02/Task04_036.java
+++ src/week02/Task04_036.java
@@ -1,4 +1,25 @@
 package week02;
+/**
+ * The traffic light for pedestrians is programmed as follows: at the beginning of each hour for three
+ * minutes a green signal is illuminated, then for two minutes - red, for three minutes - again green, etc.
+ * Given a real number t, meaning the time in minutes, elapsed since the beginning of the next hour.
+ * Determine the signal of what color is lit for pedestrians at this moment.
+ */
 
 public class Task04_036 {
+
+    public static void main(String[] args) {
+        int t = (int) (Math.random() * 100);
+        System.out.println("t = " + t + "; Traffic light is " + printcolor(t));
+    }
+
+    public static String printcolor(int t) {
+        String color;
+        if (t % 5 < 3) {
+            color = "Green";
+        } else {
+            color = "Red";
+        }
+        return color;
+    }
 }
