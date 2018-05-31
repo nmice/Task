6e5806b7225a353eb155e2d78ba92a14978c8686
nmice <n.mice@mail.ru>
2018-05-31 12:52:20 +0300

    Task05_064 done

---
+++ src/week03/Task05_064.java
@@ -0,0 +1,30 @@
+package week03;
+/**
+ * In the area of ​​12 districts. The number of inhabitants (in thousands) and the area (in km 2) of each
+ * district are known. Determine the average population density in the region as a whole. *
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task05_064 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        double peoples = 0, areas = 0;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println("Inhabitants in " + i + " district (in thousands): ");
+            double inhabitants = scan.nextDouble();
+            System.out.println("Area of " + i + " district (in km2): ");
+            double areaDistrict = scan.nextDouble();
+            peoples += inhabitants;
+            areas += areaDistrict;
+        }
+        //System.out.println(peoples + " " + areas);
+        System.out.println("Population Density is: " + calcPopulationDensity(peoples, areas) + "(thousands people/km2)");
+    }
+
+    public static double calcPopulationDensity(double peoples, double areas) {
+        return Math.rint(100.0 * peoples / areas)/100.0;
+    }
+}
