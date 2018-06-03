6a4e9cfaf17070969c459f3034c6edf5ca050cb0
nmice <n.mice@mail.ru>
2018-06-03 22:02:57 +0300

    Task06_008 w Test done

--- src/week03/Task06_008.java
+++ src/week03/Task06_008.java
@@ -4,6 +4,24 @@ package week03;
  * w\o square
  */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task06_008 {
 
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        printSquareBeforeNum(num);
+    }
+
+    public static void printSquareBeforeNum(int num) {
+        int x = (int) Math.sqrt(num);
+        int[] squareArray = new int[x];
+        for (int i = 0, j = 1, k = 0; i < x; i++, j += 2) {
+            squareArray[i] = k + j;
+            k = squareArray[i];
+        }
+        System.out.println(Arrays.toString(squareArray));
+    }
 }
---
+++ src/week03/Task06_008Test.java
@@ -0,0 +1,13 @@
+package week03;
+/**
+ * Created by Mike Neginsky on 03.06.2018.
+ */
+
+import util.TestHelper;
+
+public class Task06_008Test {
+
+    public static void main(String... args) {
+        TestHelper.checkLastString(Task06_008::main,"30","[1, 4, 9, 16, 25]");
+    }
+}
