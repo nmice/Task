2af3d907a4c3894b17c63eeba0860c6ae98b07af
n.mice <n.mice@mail.ru>
2018-06-05 23:00:09 +0300

    printstreamadapter & testhelper things... IS NOT SOLUTION

--- src/util/PrintStreamAdapter.java
+++ src/util/PrintStreamAdapter.java
@@ -1,8 +1,10 @@
 package util;
 
 import java.io.PrintStream;
+import java.util.Arrays;
 
 public class PrintStreamAdapter extends PrintStream {
+    private String[] outputStringArray;
     private String outputString;
     private int outputInt;
     private double outputDouble;
@@ -26,24 +28,24 @@ public class PrintStreamAdapter extends PrintStream {
     }
 
     @Override
-    public void print(String x) { //constructor 4 print(String) (Task06_008Test)
-        outputString = x;
-        super.print(x);
-    }
-
-    @Override //constructor 4 dollar-Task
-    public void println(double x) {
-        outputDouble = x;
-        outputString = String.valueOf(x);
-        super.print(x);
-    }
-
-    @Override
     public void println(int x) {
         outputInt = x;
         outputString = String.valueOf(x);
         super.println(x);
     }
+
+    @Override
+    public void println(double x) { //constructor 4 dollar-Task
+        outputDouble = x;
+        outputString = String.valueOf(x);
+        super.print(x);
+    }
+
+    //@Override
+    //public void print(String... x) { //constructor 4 print(String) (Task06_008Test)
+    //    outputStringArray = x;
+    //    super.print(x);
+    //}
 
     @Override
     public void print(int x) { //constructor 4 print(int) (Task06_008Test)
--- src/util/TestHelper.java
+++ src/util/TestHelper.java
@@ -17,8 +17,18 @@ public class TestHelper {
         }
     }
 
-    public static void checkAllStrings(Runnable task, String mockedInput, String... expectedOutput) {
-
+    /*public static void checkAllStrings(Runnable task, String mockedInput, String... expectedOutput) {
         //todo: implement Viktor Bugaenko
-    }
+        PrintStreamAdapter printStreamAdapter = new PrintStreamAdapter(System.out);
+        System.setOut(printStreamAdapter);
+        System.setIn(new ByteArrayInputStream(mockedInput.getBytes()));
+        task.run();//runs a task
+
+        String result = printStreamAdapter.getOutputString();
+        if (expectedOutput.trim().equals(result.trim())) {
+            System.out.println("OK");
+        } else {
+            System.err.println("FAILED, expected: " + expectedOutput + "| actual: " + result);
+        }
+}   */
 }
