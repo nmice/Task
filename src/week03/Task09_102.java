48aeb4ccdd66c22a90d969ff500d067ace13cc8e
nmice <n.mice@mail.ru>
2018-06-07 02:37:47 +0300

    begin

--- src/week03/Task09_102.java
+++ src/week03/Task09_102.java
@@ -1,12 +1,11 @@
 package week03;
-
-import java.util.Scanner;
-
 /**
  * Given a word of 15 letters. Rearrange the letters located between the k-th and the s-th
  * letters (that is, with (k + 1) -th in (s-1) -th).
  * The values ​​of k and s are entered from the keyboard, k <s.
  */
+
+import java.util.Scanner;
 
 public class Task09_102 {
 
@@ -14,29 +13,23 @@ public class Task09_102 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the word of 15 letters:");
         String word = scanner.nextLine();
-
         while (word.length() != 15) {
             System.out.println("15 letters, I said...:");
             word = scanner.nextLine();
         }
-
         System.out.println("Enter number of letter \"k\"");
         int k = scanner.nextInt();
-
         System.out.println("Enter number of letter \"s\"(s>k)");
         int s = scanner.nextInt();
-
         System.out.println("Turn the word from \"k\" to \"s\": " + invertLetters(word, k, s));
     }
 
     public static String invertLetters(String word, int k, int s) {
         String movedSymbol, invertedString = "", result;
-
         for (int i = s; i - k >= 0; i--) {
             movedSymbol = word.substring(i - 1, i);
             invertedString = invertedString + movedSymbol;
         }
-
         if (k == 1) {
             result = invertedString + word.substring(s, word.length());
         } else if (s == 15) {
@@ -44,7 +37,6 @@ public class Task09_102 {
         } else {
             result = word.substring(0, k - 1) + invertedString + word.substring(s, word.length());
         }
-
         return result;
     }
 }
--- src/week03/Task09_166.java
+++ src/week03/Task09_166.java
@@ -1,6 +1,27 @@
 package week03;
+/**
+ * Given the sentence. Swap his first and last word.
+ */
+
+import java.util.Scanner;
+
 public class Task09_166 {
 
     public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the sentence:");
+        String sentence = scanner.nextLine();
+        System.out.println("Sentence after swapping of the first and last words: " + swapWords(sentence));
+    }
+
+    public static String swapWords(String sentence) {
+        String newSentence = sentence.trim(); //remove " "
+        String[] wordsArray = newSentence.split(" ");
+
+
+        char[] chars = new char[newSentence.length()];// new char massive wo " "
+        newSentence.getChars(0, 5, chars, 0);
+
+        return newSentence;
     }
 }
