ee7d72c737b4a30f34480051f5eff2da59fdabe4
nmice <n.mice@mail.ru>
2018-06-10 21:03:24 +0300

    Task11_158 & Task11_158Test Done! git commit -m Task11_158

--- src/week03/Task11_158.java
+++ src/week03/Task11_158.java
@@ -12,31 +12,61 @@ public class Task11_158 {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a value of massive elements:");
-        int massiveLength = scanner.nextInt();
-        int[] mas = new int[massiveLength];
-        System.out.println("Enter " + massiveLength + " numbers - elements of massive:");
+        int arrayLength = scanner.nextInt();
+        int[] array = new int[arrayLength];
+        System.out.println("Enter " + arrayLength + " numbers - elements of massive:");
         int i = 0;
-        while (i < massiveLength) {
-            mas[i] = scanner.nextInt();
+        while (i < arrayLength) {
+            array[i] = scanner.nextInt();
             i++;
         }
-        System.out.println("Your Array: " + "\r\n" + Arrays.toString(mas));
-        invertMas(mas);
+        System.out.println("Your Array: " + "\r\n" + Arrays.toString(array));
 
-        public static int[] invertMas (int... mas){
-            String movedSymbol, invertedString = "", result;
-            for (int i = s; i - k >= 0; i--) {
-                movedSymbol = word.substring(i - 1, i);
-                invertedString = invertedString + movedSymbol;
+        //reverse array wo duplicate
+        int k = arrayLength - 1;//variable for index of the digit to be checked
+        int l = 0;//variable for check digit index
+        int newJ = 0;//variable for index of noDuplicate
+        int[] reverseArrayWoDuplicate = new int[arrayLength];
+        System.out.println("Created a twin-array of Your Array - reverseArrayWoDuplicate");
+        boolean noDuplicate = array[k] != array[l];
+        while (k > 0) {
+            System.out.println(k + "-th element. Check duplicates...");
+            for (l = k - 1; l >= 0; l--) {
+                if (array[k] == array[l]) {
+                    System.out.println(array[k] + " - " + k + "-th element & " + array[l] + " - " + l + "-th element is duplicate");
+                    noDuplicate = false;
+                    break;
+                }
+                System.out.println(array[l] + " - " + l + "-th element is no duplicate to " + array[k]);
             }
-            if (k == 1) {
-                result = invertedString + word.substring(s, word.length());
-            } else if (s == 15) {
-                result = word.substring(0, k - 1) + invertedString;
-            } else {
-                result = word.substring(0, k - 1) + invertedString + word.substring(s, word.length());
+            if (noDuplicate) {
+                reverseArrayWoDuplicate[newJ] = array[k];
+                System.out.println(array[k] + " - " + k + "-th element of mas is " + reverseArrayWoDuplicate[newJ] + " - " + newJ + "-th element of masWoDuplicate");
+                newJ++;
             }
-            return result;
+            k--;
+            noDuplicate = true;
         }
+        reverseArrayWoDuplicate[newJ] = array[0];
+        System.out.println(array[0] + " - 0-th element of mas is " + reverseArrayWoDuplicate[newJ] + " - " + newJ + "-th element of masWoDuplicate");
+
+        //create new Massive wo duplicate true value of elements
+        int[] newMas = new int[newJ + 1];
+        for (int iter = 0; iter <= newJ; iter++) {
+            newMas[iter] = reverseArrayWoDuplicate[iter];
+        }
+        System.out.println("Reverse Array w/o Duplicate: " + "\r\n" + Arrays.toString(reverseArrayWoDuplicate));
+
+        //invertMas()
+        int j, movedElement, invertElement;
+        for (j = 0; j < newMas.length / 2; j++) {
+            movedElement = newMas[j];
+            invertElement = newMas[newMas.length - 1 - j];
+            newMas[j] = invertElement;
+            newMas[newMas.length - 1 - j] = movedElement;
+        }
+
+        //print result array
+        System.out.println("Result Array: " + "\r\n" + Arrays.toString(newMas));
     }
 }
--- src/week03/Task11_158Test.java
+++ src/week03/Task11_158Test.java
@@ -8,6 +8,7 @@ import util.TestHelper;
 public class Task11_158Test {
 
     public static void main(String... args) {
-        TestHelper.checkLastString(Task11_158::main,"1 2 2 4 5 6 7 8 9 9","Remove Duplicate: [1, 2, 4, 5, 6, 7, 8, 9]");
+        TestHelper.checkLastString(Task11_158::main,"7 2 2 4 5 2 6 7",
+                "Result Array: " + "\r\n" + "[2, 4, 5, 6, 7] ");
     }
 }
