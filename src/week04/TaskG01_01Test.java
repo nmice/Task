a1a7514cca70adf6f1b1783ba29b828d6ac384da
nmice <n.mice@mail.ru>
2018-06-15 01:54:42 +0300

    TaskG01_01_factorial & Test done

---
+++ src/week04/TaskG01_01Test.java
@@ -0,0 +1,14 @@
+package week04;
+/**
+ * Created by Mike Neginsky on 15.06.2018.
+ */
+
+import util.TestHelper;
+import week03.Task12_023;
+
+public class TaskG01_01Test {
+
+    public static void main(String... args) {
+        TestHelper.checkAllStrings(TaskG01_01_factorial::main, "6","720", "720");
+    }
+}
--- src/week04/TaskG01_01_factorial.java
+++ src/week04/TaskG01_01_factorial.java
@@ -1,13 +1,24 @@
 package week04;
+/**
+ * Your program should take integer value from keyboard, and print factorial for this number.
+ * There should be two solutions: calculating factorial using iterative solution (using loop),
+ * and recursive solution (without loop). Inside of your methods you have to use BigInteger class for storing numbers
+ * (since factorial values can be huge) Both solution should be covered with unit-tests.
+ */
 
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class TaskG01_01_factorial {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+        //1st solution:
         System.out.println(getFactorial(num));
+        //2nd solution:
+        BigInteger factorial = new BigInteger("1");
+        getFactorial1(factorial, num);
+
     }
 
     public static BigInteger getFactorial(int num) {
@@ -17,5 +28,16 @@ public class TaskG01_01_factorial {
             factorial = factorial.multiply(iBig);
         }
         return factorial;
+    }
+
+    public static void getFactorial1(BigInteger factorial , int num) {
+        if (num==0) {
+            System.out.println(factorial);
+        } else {
+            BigInteger numBig = new BigInteger(String.valueOf(num));
+            factorial = factorial.multiply(numBig);
+            num--;
+            getFactorial1(factorial, num);
+        }
     }
 }
