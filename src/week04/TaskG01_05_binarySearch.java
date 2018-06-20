8f4361e725b3d8cc2046aa3b452a4c4385ff23d4
nmice <n.mice@mail.ru>
2018-06-20 17:16:01 +0300

    update

--- src/week04/TaskG01_05_binarySearch.java
+++ src/week04/TaskG01_05_binarySearch.java
@@ -15,17 +15,18 @@ public class TaskG01_05_binarySearch {
         System.out.println("Enter a sorted sequence of words:");
         String words = scanner.nextLine();
         Comparable[] array = words.split(" ");
-        Comparable[] array1 = array;
         System.out.println("Enter the search word:");
-        String searchWord = scanner.nextLine();
+        Comparable searchWord = scanner.nextLine();
         System.out.println("Your Array: " + "\r\n" + Arrays.toString(array));
         System.out.println(searchWord + " - is the " + linearSearch(array, searchWord) + " word in your Array");
         System.out.println(searchWord + " - is the " + binarySearch(array, searchWord) + " word in your Array");
     }
 
-    public static String linearSearch(Comparable[] array, String searchWord) {
+    public static String linearSearch(Comparable[] array, Comparable searchWord) {
         int num = -1;
         for (int i = 0; i < array.length - 1; i++) {
+            System.out.println("check...linear");
+
             if (array[i].equals(searchWord)) {
                 num = i + 1;
                 break;
@@ -38,29 +39,27 @@ public class TaskG01_05_binarySearch {
         return numS;
     }//end of method
 
-    public static String binarySearch(Comparable[] array, String searchWord) {
-        int num = -1;
+    public static String binarySearch(Comparable[] array, Comparable searchWord) {
+        int a = (array.length - 1) / 2;
+        int k = 2;
 
-        int a = array.length;
-
-        //for (int i = 0; i < array.length - 1; i++) {
-        if (array[a / 2].compareTo(searchWord) > 0) {
-            a = a / 2 - a / 4;
-        }// end if
-        if (array[a / 2].compareTo(searchWord) < 0) {
-            a = a / 2 + a / 4;
-        }// end if
-        if (array[a / 2].compareTo(searchWord) == 0) {
-            num = a;
-        }// end if
-
-    String numS = "" + num;
-        if(numS.equals("-1"))
-
-    {
-        numS = "Not Found";
-    }
+        while (array[a].compareTo(searchWord) != 0) {
+            if (k > array.length * 2) {
+                String numS = "Not Found";
+                break;
+            }
+            if (array[a].compareTo(searchWord) > 0) {
+                a = a - a / k;
+                System.out.println("check...binary-");
+            }// end if
+            if (array[a].compareTo(searchWord) < 0) {
+                a = a + a / k;
+                System.out.println("check...binary+");
+            }// end if
+            k *= 2;
+        }// end of loop
+        String numS = "" + a + 1;
         return numS;
-}//end of method
+    }//end of method
 
 }
