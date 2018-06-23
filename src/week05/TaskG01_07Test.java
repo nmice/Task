fbd4d7f9b47e43828348db5fa6dd0eb851124e43
nmice <n.mice@mail.ru>
2018-06-23 06:46:48 +0300

    TaskG01_07Test Done, TaskG01_06 chek methods DONE

---
+++ src/week05/TaskG01_07Test.java
@@ -0,0 +1,15 @@
+package week05;
+/**
+ * Created by Mike Neginsky on 15.06.2018.
+ */
+
+import util.TestHelper;
+import week04.TaskG01_01_factorial;
+
+public class TaskG01_07Test {
+
+    public static void main(String... args) {
+        TestHelper.checkAllStrings(TaskG01_07_Merge_sorted_arrays::main, "","New array is 16 elements",
+                "[1, 2, 3, 4, 5, 6, 7, 7, 9, 10, 11, 12, 13, 15, 15, 20]");
+    }
+}
--- src/week05/TaskG01_07_Merge_sorted_arrays.java
+++ src/week05/TaskG01_07_Merge_sorted_arrays.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class TaskG01_07_Merge_sorted_arrays {
 
-    public static void main(String[] args) {
-        int[] array1 = {1, 2, 3, 5, 7, 9, 12, 14};
-        int[] array2 = {4, 6, 8, 10, 11, 13, 15, 20};
+    public static void main(String... args) {
+        int[] array1 = {1, 2, 3, 5, 7, 9, 12, 15};
+        int[] array2 = {4, 6, 7, 10, 11, 13, 15, 20};
         System.out.println(Arrays.toString(mergeSortedArrays(array1, array2)));
-        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{1, 2, 3}, new int[]{})));
-        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{}, new int[]{1, 2, 3})));
-        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{1, 2, 3}, new int[]{100, 101, 102})));
-        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{100, 101, 102}, new int[]{1, 2, 3})));
-        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{7, 8, 8}, new int[]{8, 10, 11})));
+//        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{1, 2, 3}, new int[]{})));
+//        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{}, new int[]{1, 2, 3})));
+//        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{1, 2, 3}, new int[]{100, 101, 102})));
+//        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{100, 101, 102}, new int[]{1, 2, 3})));
+//        System.out.println(Arrays.toString(mergeSortedArrays(new int[]{7, 8, 8}, new int[]{8, 10, 11})));
     }
 
     public static int[] mergeSortedArrays(int array1[], int array2[]) {
