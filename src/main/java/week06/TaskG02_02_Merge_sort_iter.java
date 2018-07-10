ffc5902fd6e8dcf25731ee647b23bdd45ee730c4
n.mice <n.mice@mail.ru>
2018-07-10 23:58:35 +0300

    try to mergeSortIter - not complete, continue work

--- src/main/java/week06/TaskG02_02_Merge_sort_iter.java
+++ src/main/java/week06/TaskG02_02_Merge_sort_iter.java
@@ -6,6 +6,8 @@ package week06;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class TaskG02_02_Merge_sort_iter {
 
     public static void main(String... args) {
@@ -14,18 +16,48 @@ public class TaskG02_02_Merge_sort_iter {
         String words = scanner.nextLine();
         Comparable[] array = words.split(" ");
         System.out.println("Your Array: " + "\r\n" + Arrays.toString(array));
-        System.out.println("Merge Sort Array: " + "\r\n" + Arrays.toString(mergeSort(array)));
+        System.out.println("Merge Sort Array: " + "\r\n" + Arrays.toString(mergeSortRec(array)));
+        System.out.println("Merge Sort Array: " + "\r\n" + Arrays.toString(mergeSortIter(array)));
     }
 
-    public static Comparable[] mergeSort(Comparable[] array) {
+    public static Comparable[] mergeSortRec(Comparable[] array) {
         if (array.length < 2) {
             return array;
         } else {
             Comparable[] leftHalf = Arrays.copyOfRange(array, 0, array.length / 2);
             Comparable[] rightHalf = Arrays.copyOfRange(array, array.length / 2, array.length);
-            return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+            return mergeSortedArrays(mergeSortRec(leftHalf), mergeSortRec(rightHalf));
         }//end of method
     }//end of method
+
+
+    public static Comparable[] mergeSortIter(Comparable[] array) {
+        /*
+        Допустим есть массив из 9 элементов
+        Нужно делить его на части пока не останется 2 или 1 элемент,
+        а в смержином массиве не будет того же кол-ва элементов, что и в изначальном
+        1 итерация - создаем 2 новых массива:
+        левая часть - от 0-го до a.length.2/2 = 4   -  это элементы 0,1,2,3 массива
+        правая часть - от a.length.2/2 (4-го) до конечного элемента a.length-1 -  это элементы 4,5,6,7,8 массива
+        -Проверяем условие - если 1 элемент - оставляем,
+        -если 2 элемента - если 1>2-го то свапаем, если нет, оставляем
+        -Если больше элементов, то следующая итерация
+
+        Длины новых массивов - от 1 до a.length/2, каждый раз в 2 раза больше
+        Номера массивов - перебор от 0-го до последнего, с шагом номер эл-та+ 2х длина массива в проходке
+        Взяли первый массив(от номера эл-та в итерации до номер + длина массива в итерации)
+        Взяли второй массив(от номера эл-та в итерации + длина массива в итерации до номер + 2x длина массива в итерации)
+         */
+        for (int newArraysLength = 1; newArraysLength <= array.length / 2; newArraysLength *= 2) {
+            for (int newArrayIndex = 0; newArrayIndex < array.length; newArrayIndex += newArraysLength) {
+                Comparable[] leftHalf = Arrays.copyOfRange(array, newArrayIndex, newArrayIndex + newArraysLength);
+                Comparable[] rightHalf = Arrays.copyOfRange(array, newArrayIndex + newArraysLength, newArrayIndex + 2 * newArraysLength);
+                mergeSortedArrays(leftHalf, rightHalf);
+            }
+        }
+        return resultArray;
+    }
+
 
     public static Comparable[] mergeSortedArrays(Comparable[] array1, Comparable[] array2) {
         Comparable[] resultArray = new Comparable[array1.length + array2.length];
