b5c75a7e09fa64f9592d2f29a36f870b3b240edc
n.mice <n.mice@mail.ru>
2018-07-14 18:15:00 +0300

    arrayListOwn compl

--- src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwn.java
+++ src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwn.java
@@ -1,9 +1,12 @@
 package week06;
+
+import java.util.ArrayList;
+
 /**
  * Implement the class that will have the same functionality as ArrayList (add, remove elements, get size, clear the array).
  * It’s ok to implement the functionality partially. But SOMETHING SHOULD WORK
  */
 
 public class TaskG02_04_ArrayList {
-
+    ArrayList
 }
---
+++ src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwnTestDrive.java
@@ -0,0 +1,4 @@
+package week06.TaskG02_04_ArrayList;
+
+public class ArrayListOwnTestDrive {
+}
