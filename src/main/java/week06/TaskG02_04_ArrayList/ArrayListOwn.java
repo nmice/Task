ca1b9773f367e282ea958fd75bf348e168b1398e
n.mice <n.mice@mail.ru>
2018-07-14 18:15:19 +0300

    arrayListOwn compl

--- src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwn.java
+++ src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwn.java
@@ -1,12 +1,38 @@
-package week06;
-
-import java.util.ArrayList;
+package week06.TaskG02_04_ArrayList;
 
 /**
  * Implement the class that will have the same functionality as ArrayList (add, remove elements, get size, clear the array).
  * It’s ok to implement the functionality partially. But SOMETHING SHOULD WORK
  */
 
-public class TaskG02_04_ArrayList {
-    ArrayList
+public class ArrayListOwn<T> {
+    private Object[] array = new Object[10];
+    private int pointer = 0;
+
+    public void add(T elem) {
+        if (pointer == array.length - 1) {
+            Object[] newArray = new Object[array.length * 2];
+            System.arraycopy(array, 0, newArray, 0, pointer);
+            array = newArray;
+        }
+        array[pointer++] = elem;
+    }
+
+    public void removeElement(int index) {
+        for (int i = index; i < pointer; i++) {
+            array[i] = array[i + 1];
+        }
+        array[pointer--] = null;
+    }
+
+    public int getSize() {
+        return pointer;
+    }
+
+    public void clearTheArray() {
+        for (int i = 0; i < pointer; i++) {
+            array[i] = null;
+        }
+    }
+
 }
--- src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwnTestDrive.java
+++ src/main/java/week06/TaskG02_04_ArrayList/ArrayListOwnTestDrive.java
@@ -1,4 +1,31 @@
 package week06.TaskG02_04_ArrayList;
 
+import java.util.Arrays;
+
 public class ArrayListOwnTestDrive {
+
+    public static void main(String[] args) {
+        ArrayListOwn arrayListOwn = new ArrayListOwn();
+        System.out.println(arrayListOwn.toString());
+
+        arrayListOwn.add("Hello");
+        System.out.println(arrayListOwn);
+
+        arrayListOwn.add("World");
+        System.out.println(arrayListOwn);
+
+        arrayListOwn.add("!");
+        System.out.println(arrayListOwn);
+
+        arrayListOwn.add("!");
+        System.out.println(arrayListOwn);
+
+        arrayListOwn.removeElement(3);
+        System.out.println(arrayListOwn);
+
+        System.out.println(arrayListOwn.getSize());
+
+        arrayListOwn.clearTheArray();
+        System.out.println(arrayListOwn);
+    }
 }
