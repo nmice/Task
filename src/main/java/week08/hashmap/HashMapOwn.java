565d01327f195aa5f01a3fd5381c73ec63c337e5
n.mice <n.mice@mail.ru>
2018-08-11 23:20:37 +0300

    upd - put & get methods done

--- src/main/java/week08/hashmap/HashMapOwn.java
+++ src/main/java/week08/hashmap/HashMapOwn.java
@@ -1,9 +1,6 @@
 package week08.hashmap;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public final class HashMapOwn<K, V> implements Map<K, V> {
 
@@ -56,13 +53,59 @@ public final class HashMapOwn<K, V> implements Map<K, V> {
 
     @Override
     public V get(Object key) {
+        int index = 0;
+        if (key != null) {
+            index = key.hashCode() % array.length;
+        }
+        if (array[index] != null) {
+            List<Node<K, V>> nodeList = (List) array[index];
+            for (Node<K, V> node : nodeList) {
+                if (key == null && node.key == null || key.equals(node.key)) {
+                    return node.value;
+                }
+            }
+        }
         return null;
     }//TODO
 
     @Override
     public V put(K key, V value) {
+        //1) Создать эл-т Node<K,V> c ключом и значением
+        Node<K, V> newNode;
+        //2) Найти индекс ячейки для помещения Node
+        int index = 0;
+        if (key == null) {
+            newNode = new Node(0, key, value, null);
+        } else {
+            newNode = new Node(key.hashCode(), key, value, null);
+            index = key.hashCode() % array.length;
+        }
+        //2.1) Если ячейка пуста, Создать новый связный список,
+        if (array[index] == null) {
+            List nodeList = new LinkedList();
+            //2.1.1) Поместить список в ячейку с найденным индексом
+            array[index] = nodeList;
+            //2.1.2) Поместить эл-т Node в связный список
+            nodeList.add(newNode);
+            //2.2) Если ячейка не пуста,
+        } else {
+            //2.2.1) если оба Key==null или совпадают Key, заменить в эл-те Node с ключом key значение value на переданное в метод значение value
+            List<Node<K, V>> nodeList = (LinkedList) array[index];
+            for (Node<K, V> node : nodeList) {
+                if (key == null && node.key == null || key.equals(node.key)) {
+                    V oldValue = node.value;
+                    node.value = value;
+                    return oldValue;
+                }
+            }
+            //2.2.2) если не совпадают Key, добавить эл-т Node в имеющийся связный список,
+            nodeList.add(newNode);
+        }
+        //3) Увеличить счетчик записей
+        size++;
         return null;
     }//TODO
+
 
     @Override
     public V remove(Object key) {
@@ -76,7 +119,10 @@ public final class HashMapOwn<K, V> implements Map<K, V> {
 
     @Override
     public void clear() { //TODO
-
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
     }
 
     @Override
@@ -96,6 +142,66 @@ public final class HashMapOwn<K, V> implements Map<K, V> {
 
     @Override
     public final String toString() {
-        return "";
+        StringBuilder result = new StringBuilder();
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] != null) {
+                for (Node<K, V> node : (List<Node<K, V>>) array[i]) {
+//                    if (node != null) {
+                    result.append(result.length() == 0 ? "" : ", ");
+                    result.append(node.toString());
+//                    }
+                }
+            }
+        }
+        return "{" + result.toString() + "}";
+    }
+
+    static class Node<K, V> implements Map.Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+        Node<K, V> first;
+
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+
+        public final String toString() {
+            return key + "=" + value;
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (o instanceof Map.Entry) {
+                Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+                if (Objects.equals(key, e.getKey()) &&
+                        Objects.equals(value, e.getValue()))
+                    return true;
+            }
+            return false;
+        }
     }
 }
--- src/main/java/week08/hashmap/HashMapOwnTestDrive.java
+++ src/main/java/week08/hashmap/HashMapOwnTestDrive.java
@@ -1,16 +1,52 @@
 package week08.hashmap;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapOwnTestDrive {
 
     public static void main(String[] args) {
+        System.out.println("!HASHMAP ORIGIN!");
+        Map<String, String> booksOrigin = new HashMap<String, String>();
+        System.out.println("HashMap books is empty - is a " + booksOrigin.isEmpty());
+        System.out.println("Number of HashMap books elements is " + booksOrigin.size());
+        System.out.println(booksOrigin);
+        System.out.println(booksOrigin.get(null));
+        booksOrigin.put("AaAaAa", "NEG");
+        booksOrigin.put("AaAaAa", "NEG");
+        booksOrigin.put("AaAaAa", "NEG1");
+        booksOrigin.put("BBBBBB", "NEG1");
+        System.out.println(booksOrigin.put("BBB", "NEG2"));
+        booksOrigin.put("BBB", "NEG2");
+        booksOrigin.put(null, "NEG3");
+        System.out.println(booksOrigin.put(null, null));
+        System.out.println(booksOrigin);
+        System.out.println(booksOrigin.size());
+        System.out.println(booksOrigin.get("AaAaAa"));
+        System.out.println(booksOrigin.get("AaA"));
+        System.out.println(booksOrigin.get(null));
+
+
+        System.out.println();//spacebar
+
+        System.out.println("!HASHMAP OWN!");
         Map<String, String> books = new HashMapOwn<String, String>();
         System.out.println("HashMap books is empty - is a " + books.isEmpty());
         System.out.println("Number of HashMap books elements is " + books.size());
         System.out.println(books);
-
+        System.out.println(books.get(null));
+        books.put("AaAaAa", "NEG");
+        books.put("AaAaAa", "NEG");
+        books.put("AaAaAa", "NEG1");
+        books.put("BBBBBB", "NEG1");
+        System.out.println(books.put("BBB", "NEG2"));
+        books.put("BBB", "NEG2");
+        books.put(null, "NEG3");
+        System.out.println(books.put(null, null));
+        System.out.println(books);
+        System.out.println(books.size());
+        System.out.println(books.get("AaAaAa"));
+        System.out.println(books.get("AaA"));
+        System.out.println(books.get(null));
     }
-
-
 }
