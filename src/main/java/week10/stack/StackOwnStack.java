61a5468cb2556c808770ae2cb700676c8dbb0cff
n.mice <n.mice@mail.ru>
2018-08-22 23:16:43 +0300

    JUnit test for Task_StackOwn Done

--- src/main/java/week10/stack/StackOwnStack.java
+++ src/main/java/week10/stack/StackOwnStack.java
@@ -1,37 +1,35 @@
 package week10.stack;
 
 
-import week07.Task_LinkedList.LinkedListOwn;
-
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
-public class StackOwnLinkedList<E> implements IStack<E> {
-    Deque<E> list = new LinkedList<>();
+public class StackOwnStack<E> implements IStack<E> {
+    Stack<E> stack = new Stack<>();
 
     @Override
     public E push(E item) {
-        list.push(item);
+        stack.push(item);
         return item;
     }
 
     @Override
     public E pop() {
-        return list.pop();
+        return stack.pop();
     }
 
     @Override
     public E peek() {
-        return list.peek();
+        return stack.peek();
     }
 
     @Override
     public boolean empty() {
-        return list.isEmpty();
+        return stack.isEmpty();
     }
 
     public String toString(){
-        return list.toString();
+        return stack.toString();
     }
 }
--- src/test/java/week10/stack/StackOwnTest.java
+++ src/test/java/week10/stack/StackOwnTest.java
@@ -3,53 +3,69 @@ package week10.stack;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Stack;
-
 import static org.junit.Assert.*;
 
 public class StackOwnTest {
 
     private StackOwn<Integer> testSO = null;
-    private StackOwnLinkedList<Integer> testSOLL = null;
+    private StackOwnStack<Integer> testSOS = null;
 
     @Before
     public void before() {
         testSO = new StackOwn<>();
-        testSOLL = new StackOwnLinkedList<>();
+        testSOS = new StackOwnStack<>();
     }
 
     @Test
     public void push() {
         testSO.push(123);
-        testSOLL.push(123);
+        testSOS.push(123);
         String actual = testSO.toString();
-        String expected = testSOLL.toString();
+        String expected = testSOS.toString();
         assertEquals(expected, actual);
         testSO.push(456);
-        testSOLL.push(456);
+        testSOS.push(456);
         actual = testSO.toString();
-        expected = testSOLL.toString();
+        expected = testSOS.toString();
         assertEquals(expected, actual);
     }
 
     @Test
     public void pop() {
+        testSO.push(123);
+        testSOS.push(123);
+        testSO.push(456);
+        testSOS.push(456);
+        testSO.push(789);
+        testSOS.push(789);
+        Integer actualOne = testSO.pop();
+        Integer expectedOne = testSOS.pop();
+        assertEquals(expectedOne, actualOne);
+        String actualTwo = testSO.toString();
+        String expectedTwo = testSOS.toString();
+        assertEquals(expectedTwo, actualTwo);
     }
 
     @Test
     public void peek() {
-
+        testSO.push(123);
+        testSOS.push(123);
+        testSO.push(456);
+        testSOS.push(456);
+        Integer actual = testSO.peek();
+        Integer expected = testSOS.peek();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void empty() {
         boolean actual = testSO.empty();
-        boolean expected = testSOLL.empty();
+        boolean expected = testSOS.empty();
         assertEquals(expected, actual);
         testSO.push(123);
-        testSOLL.push(123);
+        testSOS.push(123);
         actual = testSO.empty();
-        expected = testSOLL.empty();
+        expected = testSOS.empty();
         assertEquals(expected, actual);
     }
 }
