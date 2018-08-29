cabc52ab0741dec04816960dfebb6cbbbb22c0b3
nmice <n.mice@mail.ru>
2018-08-30 00:42:49 +0300

    QuickSort done wo test

--- src/main/java/week10/quickSort/QuickSort.java
+++ src/main/java/week10/quickSort/QuickSort.java
@@ -2,38 +2,34 @@ package week10.quickSort;
 
 import java.util.Arrays;
 
-/*
-Берем массив, выбираем у него опорный элемент
-Запускаем цикл от 0го эл-та до нашего опорного
-Сравниваем данный элемент с нашим опорным
-Если данный меньше опорного, оставляем всё как есть.
-Если данный больше опорного - меняем его с опорным элементом местами
- */
-
 public class QuickSort {
 
     public static void main(String[] args) {
         Integer[] myArray = {184, 124, 187, 136, 185, 2041, 0, -100, 1, 256, 4};
         System.out.println("Your Array: " + Arrays.toString(myArray));
-        System.out.println("Sorted Array by quickSort: " + Arrays.toString(quickSortArray(myArray, 0, myArray.length - 1)));
+        System.out.println("Sorted Array by quickSort: " + Arrays.toString(quickSortArray(myArray)));
     }
 
-    public static Integer[] quickSortArray(Integer[] array, int start, int end) {
+    public static Comparable[] quickSortArray(Comparable[] array) {
+        return doSort(array, 0, array.length - 1);
+    }
+
+    public static Comparable[] doSort(Comparable[] array, int start, int end) {
         if (end - start <= 1) {
             return array;
         }
-        int indexOfBasicElement = (end - start) / 2;
         int i = start;
         int j = end;
+        int indexOfBasicElement = (i + j) / 2;
         while (i < j) {
-            while (i < indexOfBasicElement && array[i] < array[indexOfBasicElement]) {
+            while (i < indexOfBasicElement && array[i].compareTo(array[indexOfBasicElement]) == -1) { //array[i] < array[indexOfBasicElement]
                 i++;
             }
-            while (j > indexOfBasicElement && array[j] >= array[indexOfBasicElement]) {
+            while (j > indexOfBasicElement && array[j].compareTo(array[indexOfBasicElement]) >= 0) { //array[j] >= array[indexOfBasicElement]
                 j--;
             }
             if (i < j) {
-                int temp = array[i];
+                Comparable temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
                 if (i == indexOfBasicElement)
@@ -42,8 +38,8 @@ public class QuickSort {
                     indexOfBasicElement = i;
             }
         }
-        //quickSortArray(array, start, indexOfBasicElement);
-        quickSortArray(array, indexOfBasicElement+1, end);
+        doSort(array, start, indexOfBasicElement);
+        doSort(array, indexOfBasicElement + 1, end);
         return array;
     }
 }
