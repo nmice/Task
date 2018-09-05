3b9a399ad05c338160b77c5884ce8531a5b8af5e
nmice <n.mice@mail.ru>
2018-09-06 02:58:29 +0300

    upd

--- src/test/java/week10/quickSort/QuickSortTest.java
+++ src/test/java/week10/quickSort/QuickSortTest.java
@@ -3,6 +3,9 @@ package week10.quickSort;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import static java.util.Arrays.sort;
 import static org.junit.Assert.*;
 import static week10.quickSort.QuickSort.quickSortArray;
@@ -12,7 +15,8 @@ public class QuickSortTest {
     private Integer[] testIntArray2 = new Integer[100];
     private String[] testStrArray1 = new String[100];
     private String[] testStrArray2 = new String[100];
-
+    private Book[] testBookArray1 = new Book[10];
+    private Book[] testBookArray2 = new Book[10];
 
     @Before
     public void before() {
@@ -25,6 +29,19 @@ public class QuickSortTest {
             testStrArray1[i] = Long.toHexString(Double.doubleToLongBits(Math.random()));
         }
         System.arraycopy(testStrArray1, 0, testStrArray2, 0, 100);
+
+        testBookArray1[0] = new Book("Pushkin", "Eugene Onegin");
+        testBookArray1[1] = new Book("Pushkin", "The Tale about Tsar Saltan");
+        testBookArray1[2] = new Book("Gogol", "Viy");
+        testBookArray1[3] = new Book("Gogol", "Evenings on a farm near Dikanka");
+        testBookArray1[4] = new Book("Lermontov", "Sail");
+        testBookArray1[5] = new Book("Tolstoy", "War & Peace");
+        testBookArray1[6] = new Book("Dostoevskiy", "Idiot");
+        testBookArray1[7] = new Book("Gogol", "Nose");
+        testBookArray1[8] = new Book("Bradbury", "451 Fahrenheit");
+        testBookArray1[9] = new Book("Pelevin", "Generation P");
+        Comparator<Book> bookComparator = new BookAuthorComparator().thenComparing(new BookTitleComparator());
+        System.arraycopy(testBookArray1, 0, testBookArray2, 0, 10);
     }
 
     @Test
@@ -33,6 +50,8 @@ public class QuickSortTest {
         assertArrayEquals(testIntArray1, quickSortArray(testIntArray2));
         sort(testStrArray1);
         assertArrayEquals(testStrArray1, quickSortArray(testStrArray2));
+        sort(testBookArray1);
+        assertEquals(Arrays.toString(testBookArray1), Arrays.toString(quickSortArray(testBookArray2)));
     }
 
     @Test
