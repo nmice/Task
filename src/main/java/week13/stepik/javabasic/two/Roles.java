db154f7205e6b22e58c87a8a4336ef02d32e55f3
n.mice <n.mice@mail.ru>
2018-09-30 09:03:04 +0300

    upd

--- src/main/java/week13/stepik/javabasic/two/Roles.java
+++ src/main/java/week13/stepik/javabasic/two/Roles.java
@@ -2,12 +2,18 @@ package week13.stepik.javabasic.two;
 
 public class Roles {
     public static void main(String[] args) {
-        String[] roles = {"Neo", "Morpheus", "Trinity"};
+        String[] roles = {"Neo", "Trinity", "Morpheus"};
         String[] textLines = {"Neo: Help me!", "Morpheus: No Way!", "Neo: WTF, Man?", "Trinity: is Matrix", "Morpheus: Matrix?..."};
         System.out.println(printTextPerRole(roles, textLines));
     }
 
     private static String printTextPerRole(String[] roles, String[] textLines) {
+        for (int i = 0; i < textLines.length; i++) {
+            String role = textLines[i].substring(0, textLines[i].indexOf(':'));
+
+
+        }
+
         return "dc";
     }
 }
