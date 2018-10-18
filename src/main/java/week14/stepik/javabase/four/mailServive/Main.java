2c20c8c2ca863d0ef0baa19478f9f61f3fbba9d2
nmice <n.mice@mail.ru>
2018-10-18 17:25:44 +0300

    upd

--- src/main/java/week14/stepik/javabase/four/mailServive/Main.java
+++ src/main/java/week14/stepik/javabase/four/mailServive/Main.java
@@ -3,16 +3,27 @@ package week14.stepik.javabase.four.mailServive;
 public class Main {
 
     public static void main(String[] args) {
+        Sendable newMessage = new MailMessage("Greg","Mike","Hello");
+        MailService[] persons = new MailService[]{new RealMailService(), new RealMailService()};
+        MailService uMW = new UntrustworthyMailWorker(persons);
 
     }
 
 
     public static class UntrustworthyMailWorker implements MailService {
+        MailService[] bandits;
+
         public UntrustworthyMailWorker(MailService[] mailservices) {
+            if (mailservices == null){
+                bandits = null;
+            }
+            bandits = new MailService[mailservices.length];
+            System.arraycopy(mailservices, 0, bandits, 0, mailservices.length);
         }
 
         @Override
         public Sendable processMail(Sendable mail) {
+
             return null;
         }
     }
