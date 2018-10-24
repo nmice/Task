800c32c29ddc82fdc8c5790a398b4d32a1d42573
n.mice <n.mice@mail.ru>
2018-10-24 08:01:28 +0300

    upd

--- src/main/java/week16/stepik/javabase/five/InputStreamWithCharset.java
+++ src/main/java/week16/stepik/javabase/five/InputStreamWithCharset.java
@@ -1,6 +1,11 @@
 package week16.stepik.javabase.five;
 
-import week13.stepik.javabasic.two.Char;
+/**
+ * Реализуйте метод, который зачитает данные из InputStream и преобразует их в строку, используя заданную кодировку.
+ * Пример:
+ * InputStream последовательно возвращает четыре байта: 48 49 50 51.
+ * Метод, вызванный для такого InputStream и кодировки ASCII, должен вернуть строку "0123".
+ */
 
 import java.io.*;
 import java.nio.charset.Charset;
@@ -12,19 +17,13 @@ public class InputStreamWithCharset {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        BufferedReader reader = new InputStreamReader(inputStream, charset);
-        StringBuilder resBuilder = new StringBuilder("");
-        String s = reader.re
-        while (!(reader.readLine().equals(""))) {
-            Charset charFromInput = StandardCharsets.UTF_8;
-            resBuilder.append(charFromInput);
+        Reader reader = new InputStreamReader(inputStream, charset);
+        BufferedReader bufReader = new BufferedReader(reader);
+        String result = new String;
+        while (bufReader.readLine() != null) {
+            result += "\r\n"+ bufReader.readLine();
         }
-        return resBuilder.toString();
+        return result;
 // your implementation here
-
-        //Reader reader = new InputStreamReader(inputStream, "UTF-8");
-
-        //Charset charset = StandardCharsets.UTF_8;
-        //Writer writer = new OutputStreamWriter(outputStream, charset);
     }
 }
