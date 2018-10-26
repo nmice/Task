2309f34a04b209f7553aa2440724214a565fa8ee
n.mice <n.mice@mail.ru>
2018-10-27 00:50:44 +0300

    Task SerializableAnimal Done - 5th Module Done

--- src/main/java/week16/stepik/javabase/five/SerializableAnimal.java
+++ src/main/java/week16/stepik/javabase/five/SerializableAnimal.java
@@ -1,18 +1,45 @@
 package week16.stepik.javabase.five;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class SerializableAnimal {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Animal[] animals = {new Animal("Cat"), new Animal("Dog"), new Animal("Elephant"), new Animal("Cock"),
+                new Animal("Bull"), new Animal("Ant"), new Animal("Tentecles"), new Animal("Worm")};
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeInt(animals.length);
+        for (int indexOfAnimals = 0; indexOfAnimals < animals.length; indexOfAnimals++) {
+            objectOutputStream.writeObject(animals[indexOfAnimals]);
+        }
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        Animal[] animalAfterSerialize = deserializeAnimalArray(byteArrayOutputStream.toByteArray());
+        System.out.println(Arrays.toString(animalAfterSerialize));
     }
 
     public static Animal[] deserializeAnimalArray(byte[] data) {
-        // your implementation here
+        // Создать поток байтов из массива байтов
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        try (ObjectInputStream ois = new ObjectInputStream(byteArrayInputStream)) {
+            // Считать int
+            int length = ois.readInt();
+            // Создать массив объектов Animal
+            Animal[] animals = new Animal[length];
+            for (int i=0;i<length;i++){
+                // Считать часть данных - записать в массив на месо 0-го элемента
+                Animal deserAnimal = (Animal) ois.readObject();
+                animals[i]=deserAnimal;
+            }
+            return animals;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("oops...");
+        }
     }
 
-    class Animal implements Serializable {
+    private static class Animal implements Serializable {
         private final String name;
 
         public Animal(String name) {
