212514c3875513c177ccca10b36c11fff6c713cf
nmice <n.mice@mail.ru>
2018-10-27 20:39:31 +0300

    Stepic's 6.1(Generics) Done

---
+++ src/main/java/week16/stepik/javabase/six/pair/Pair.java
@@ -0,0 +1,45 @@
+package week16.stepik.javabase.six.pair;
+
+import java.util.Objects;
+
+class Pair<First,Second> {
+
+    First firstElement = null;
+    Second secondElement = null;
+
+    private Pair(First first, Second second){
+        firstElement = first;
+        secondElement = second;
+    }
+
+    public First getFirst() {
+        return firstElement;
+    }
+
+    public Second getSecond() {
+        return secondElement;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(firstElement, pair.firstElement) &&
+                Objects.equals(secondElement, pair.secondElement);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(firstElement, secondElement);
+    }
+
+    public static Pair of(Object first, Object second) {
+        return new Pair(first, second);
+    }
+
+    public boolean isPresent() {
+        return firstElement != null && secondElement!=null;
+    }
+}
