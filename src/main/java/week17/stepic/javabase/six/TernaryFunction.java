1f88f43b926228f6583d71dd58a9351512882ba6
n.mice <n.mice@mail.ru>
2018-11-01 22:48:14 +0300

    Task TernaryFunction Done

---
+++ src/main/java/week17/stepic/javabase/six/TernaryFunction.java
@@ -0,0 +1,39 @@
+package week17.stepic.javabase.six;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Давайте научимся комбинировать функции в более сложные функции. Для примера построим следующую комбинацию.
+ * Дан предикат condition и две функции ifTrue и ifFalse. Напишите метод ternaryOperator, который из них построит новую функцию,
+ * возвращающую значение функции ifTrue, если предикат выполнен, и значение ifFalse иначе.
+ * Пример использования метода (можно было все свернуть в одну строчку, но для наглядности все элементы вынесены в отдельные переменные):
+ * <p>
+ * Predicate<Object> condition = Objects::isNull;
+ * Function<Object, Integer> ifTrue = obj -> 0;
+ * Function<CharSequence, Integer> ifFalse = CharSequence::length;
+ * Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+ * <p>
+ * Результирующая функция будет для нулевых ссылок на String возвращать 0, а для ненулевых ссылок возвращать длину строки.
+ * В качестве дополнительного задания самостоятельно разберите, почему у метода ternaryOperator такая сложная сигнатура.
+ */
+
+public class TernaryFunction {
+    Predicate<Object> condition = Objects::isNull;
+    Function<Object, Integer> ifTrue = obj -> 0;
+    Function<CharSequence, Integer> ifFalse = CharSequence::length;
+    Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return (s) -> condition.test(s) ? ifTrue.apply(s) : ifFalse.apply(s);
+    }
+}
+
