2c7afff3cfd73c3e60a991e657e593a3f31932cc
n.mice <n.mice@mail.ru>
2018-11-03 04:55:47 +0300

    Task FindMinMaxInStream Done

--- src/main/java/week17/stepic/javabase/six/FindMinMaxInStream.java
+++ src/main/java/week17/stepic/javabase/six/FindMinMaxInStream.java
@@ -27,48 +27,6 @@ public class FindMinMaxInStream {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-/*        if (stream.limit(1).count() == 0) {
-            minMaxConsumer.accept(null, null);
-        } else {
-            Iterator<? extends T> iterator = stream.iterator();
-            Optional<T> prev = null;
-            Optional<T> min = null;
-            while (iterator.hasNext()) {
-                Optional<T> next = Optional.of(iterator.next());
-                if (prev != null) {
-                    if (order.compare(prev.get(), next.get()) < 0) {
-                        min = prev;
-                    } else {
-                        min = next;
-                    }
-                }
-                prev = next;
-            }
-            minMaxConsumer.accept(min.get(), stream.max(order).get());
-        }*/
-
-        // your implementation here
-        /*
-                if (stream.limit(1).count() == 0) {
-            minMaxConsumer.accept(null, null);
-        } else {
-            Object[] max = new Object[1];
-            Object[] min = new Object[1];
-            max[0] = null;
-            min[0] = null;
-            Iterator<? extends T> iterator = stream.iterator();
-            while (iterator.hasNext()) {
-                if (max[0] == null) max[0] = iterator.next();
-                if (min[0] == null) min[0] = iterator.next();
-                int t = order.compare((T) max[0], iterator.next());
-                if (t < 0) max[0] = iterator.next();
-                t = order.compare((T) min[0], iterator.next());
-                if (t > 0) min[0] = iterator.next();
-            }
-            minMaxConsumer.accept((T) min[0], (T) max[0]);
-        }
-         */
         Object[] minAndMax = {null,null};
         stream.forEach(element -> {
             if (minAndMax[0] == null) minAndMax[0] = element;
