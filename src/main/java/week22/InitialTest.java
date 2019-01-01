d2c17b687b976f44f210f94df7bdddf08a9adee1
nmice <n.mice@mail.ru>
2019-01-02 02:03:35 +0300

    initialTest done

--- src/main/java/week22/InitialTest.java
+++ src/main/java/week22/InitialTest.java
@@ -1,6 +1,48 @@
 package week22;
 
 public class InitialTest {
+    public static void main(String[] args) {
+        Child c = null;
+        Parent p = null;
+        Parent pc = null;
+        System.out.println(c.value);
+        System.out.println(p.value);
+        System.out.println(pc.value);
+        c = new Child();
+        p = new Parent();
+        pc = new Child();
+        System.out.println(c.value);
+        System.out.println(p.value);
+        System.out.println(pc.value);
+        System.out.println(c.instanceValue);
+        System.out.println(p.instanceValue);
+        System.out.println(pc.instanceValue);
+    //1   30   30
+    }
+}
+
+class Child extends Parent{
+    public static int value = 1;
+    public int instanceValue = 11;
+    static {
+        System.out.println("Static block of init in Child");
+    }
+    public Child (){
+        System.out.println("Constructor of Child");
+    }
+}
+
+class Parent {
+    public static int value = 30;
+    public int instanceValue = 330;
+    static {
+        System.out.println("Static block of init in Parent");
+    }
+    public Parent (){
+        System.out.println("Constructor of Parent");
+    }
+
+
 
 }
 
